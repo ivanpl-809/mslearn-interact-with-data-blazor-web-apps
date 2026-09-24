cfb56d2c5f82b48cfdab9e1e08602120860bf78b
ivan-pl809 <pon20007@byui.edu>
2026-09-23 19:37:02 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using BlazingPizza.Data;
+using BlazingPizza.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<OrderState>();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+  builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 var app = builder.Build();
 
@@ -10,6 +16,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.UseStaticFiles();
 app.UseRouting();
 
@@ -17,4 +24,17 @@ app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
+
 app.Run();
+
+
